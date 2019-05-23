39cddc12037b950a1422084c20e37773b7ac42fe
Ayush Gupta <ayushgx@gmal.com>
2019-05-23 23:56:44 +0530

    improved groups/periods sticky bars
    - auto alignment of element box in element wrapper now
    - added more comments
    - separated variables in a file

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
   for(var i=1;i<=11;i++){
     ptable_grid+='<div class="ptable-row">';
     for(var j=1;j<=18;j++){
-      ptable_grid+='<div class="ptable-element"></div>';
+      ptable_grid+='<div class="ptable-element-wrapper"><div class="ptable-element"><div class="ptable-element__top-wrapper"><div class="ptable-element__atomic-num">51</div><div class="ptable-element__radioactive"></div></div><div class="ptable-element__element-symbol">Sb</div><div class="ptable-element__element-name">Antimony</div></div></div>';
     }
     ptable_grid+='</div>';
   }
