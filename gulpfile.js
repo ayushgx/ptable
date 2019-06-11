650511ad0b58bf383fb232775cd70dea94192f07
Ayush Gupta <ayushgx@gmal.com>
2019-06-11 17:55:31 +0530

    More improvements
    - made gulp build task work properly
    - added favicon meta

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var useref = require('gulp-useref');
-var terser = require('gulp-terser');
+var terser = require('gulp-terser');//uglify-es supports es6 but not maintained therefore used gulp-terser
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var del = require('del');
 var autoprefix = require('gulp-autoprefixer');
+var runSequence = require('gulp4-run-sequence');
+var rcs = require('gulp-rcs');//scramble names
+var beautify = require('gulp-beautify');
 
+gulp.task('clean:dist', function(cb){
+    return (del.sync('dist'), cb());
+})
 gulp.task('sass', function () {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
@@ -16,24 +22,40 @@ gulp.task('sass', function () {
 gulp.task('useref', function () {
     return gulp.src('src/*.html')
         .pipe(useref())
+
         .pipe(gulpIf('*.js', terser()))
         .pipe(gulpIf('*.css', autoprefix({
             cascade: false
         })))
         .pipe(gulpIf('*.css', cssnano()))
+
+        // .pipe(gulpIf('*.html', beautify.html({ indent_size: 2 })))
+        // .pipe(gulpIf('*.css', beautify.css({ indent_size: 2 })))
+        // .pipe(gulpIf('*.js', beautify({ indent_size: 2 })))
+        // .pipe(rcs())
+
         .pipe(gulp.dest('dist'))
 })
 
-// gulp.task('fonts', function(){
-//     return gulp.src('src/fonts/**/*')
-//         .pipe(gulp.dest('dist/fonts'))
-// })
+gulp.task('images', function (cb) {
+    return (gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist/img')), cb())
+})
 
-gulp.task('images', function () {
-    return gulp.src('src/img/**/*')
-        .pipe(gulp.dest('dist/img'))
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
 })
 
 gulp.task('watch', function () {
     gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+})
+
+gulp.task('build', function(callback){
+    runSequence(
+        'clean:dist',
+        'sass',
+        ['useref', 'images', 'fonts'],
+        callback
+    )
 })
---
+++ src/js/ptable/desktop.js
@@ -0,0 +1,73 @@
+if (!is_touch_device()) {
+  $(document).ready(function () {
+    var viewport = document.querySelector(".ptable-wrapper__ptable");
+    var content = document.querySelector(".inner-scroll-wrapper");
+
+    var groups = document.querySelector(".ptable-groups");
+    var periods = document.querySelector(".ptable-periods");
+
+    var period_group_size = $('.ptable-groups-wrapper').height();
+    var top_bar_height = $('.top-bar').height();
+
+    var ptable_grid = $('.ptable');
+    offset = ptable_grid.offset()
+    ptableOffsetTop = offset.top - $(document).scrollTop();
+    ptableOffsetLeft = offset.left - $(document).scrollLeft();
+
+    var sb = new ScrollBooster({
+      viewport: viewport,
+      content: content,
+      bounce: true,
+      bounceForce: .1,
+      textSelection: false,
+      friction: 0.14,
+      emulateScroll: true,
+
+      onUpdate: function (data) {
+
+        content.scrollLeft = data.position.x;
+        content.scrollTop = data.position.y;
+
+        content.style.transform = `translate(${-data.position.x}px,  ${-data.position.y}px)`;
+
+        let ptable_grid = $('.ptable');
+        offset = ptable_grid.offset()
+        ptableOffsetTop = offset.top - $(document).scrollTop();
+        ptableOffsetLeft = offset.left - $(document).scrollLeft();
+
+        groups.style.left = (ptableOffsetLeft - period_group_size) + "px";
+        periods.style.top = (ptableOffsetTop - period_group_size - top_bar_height) + "px";
+
+      },
+    });
+
+    //==================================
+    //  adding overlay-scroll libray
+    //==================================
+    $('.ptable-wrapper__ptable').overlayScrollbars({
+      className: "os-theme-dark"
+    });
+
+    //prevent click while dragging ptable
+    //idea is to find initial and final position of mouse during mousedown and mouseup events;
+    //if there is no significance difference b/w positions then simply register the click.
+    const drag_click_threshold = 5;
+    $('.ptable-element').each(function (index, obj) {
+      let ptable_element = $(obj);
+      let x1, y1, x2, y2;
+      ptable_element.mousedown(function (e) {
+        x1 = e.pageX;
+        y1 = e.pageY;
+        // alert($(this).find('.ptable-element__element-name').text());
+      })
+      ptable_element.mouseup(function (e) {
+        x2 = e.pageX;
+        y2 = e.pageY;
+        // console.log('diff', 'x', Math.abs(x1 - x2), 'y', Math.abs(y1 - y2))
+        if (Math.abs(x1 - x2) <= drag_click_threshold && Math.abs(y1 - y2) <= drag_click_threshold && !ptable_element.hasClass('disabled')) {
+          // alert($(this).find('.ptable-element__element-name').text());
+        }
+      })
+    })
+  })
+}
